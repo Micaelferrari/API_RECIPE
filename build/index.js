4065e14bca0a29339e7477756617efef4aa9756b
Micael Ferrari <micaelferrari211002@gmail.com>
2024-10-27 09:58:06 -0300

    criação de novos endpoints, validações  e atualizações dos endpoints criados anteriormente

--- build/index.js
+++ build/index.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const knex_1 = __importDefault(require("knex"));
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const generatedId_1 = require("./generatedId");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
@@ -37,19 +38,27 @@ connection.raw('SELECT 1')
 });
 app.get('/recipes', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const recipe = yield connection.select('*').from('recipes');
-        res.status(200).json(recipe);
+        const recipes = yield connection.from('recipes');
+        if (recipes.length === 0) {
+            res.status(404).json({ message: "no recipes found" });
+            return;
+        }
+        res.status(200).json(recipes);
     }
     catch (err) {
-        res.status(500).json({ message: 'Erro ao buscar recipe', error: err });
+        res.status(500).json({ message: 'Erro ao buscar recipes', error: err });
     }
 }));
 app.get('/recipes/:title', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { title } = req.params;
+    if (!title) {
+        res.status(400).json({ message: "Title is required" });
+    }
     try {
         const recipe = yield connection('RECIPES').where('TITLE', 'ILIKE', `%${title}%`);
-        if (!recipe.length) {
+        if (recipe.length === 0) {
             res.status(404).json("recipe not found");
+            return;
         }
         else {
             res.json(recipe);
@@ -59,15 +68,52 @@ app.get('/recipes/:title', (req, res) => __awaiter(void 0, void 0, void 0, funct
         res.status(500).json({ message: 'Erro ao buscar recipe', error: err });
     }
 }));
-app.get('/recipes/user/:username', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { username } = req.params;
+app.delete('/recipes/:id_recipe', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id_recipe } = req.params;
+    if (!id_recipe) {
+        res.status(404).json({ message: "Id is required" });
+    }
     try {
-        const recipe = yield connection('recipes').join('USERS', 'RECIPE.USER_ID', 'USERS.ID_USERS').where('USERS.NAME_USER', username)
-            .select('RECIPES');
-        res.status(200).json(recipe);
+        const recipe = yield connection('recipes')
+            .where('id_recipe', id_recipe).first();
+        if (!recipe) {
+            res.status(404).json({ message: "Recipe not found" });
+            return;
+        }
+        yield connection('recipes')
+            .where('id_recipe', id_recipe)
+            .del();
+        res.status(200).json({ message: 'Receita deletada com sucesso!' });
     }
     catch (error) {
-        res.status(500).json({ message: 'Erro ao bsucar recipe' });
+        res.status(500).json({ message: 'Erro ao deletar a receita', error: error.message });
+    }
+}));
+app.post('/recipes', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, description, prep_time, user_id, modo_preparo } = req.body;
+    const gerarID = (0, generatedId_1.generateId)();
+    if (!title || !description || !prep_time || !user_id || !modo_preparo) {
+        res.status(404).json({ message: "Campo obrigatório" });
+        return;
+    }
+    try {
+        const userExists = yield connection('users').where('id_user', user_id).first();
+        if (!userExists) {
+            res.status(404).json({ message: 'User not found' });
+            return;
+        }
+        yield connection('recipes').insert({
+            id_recipe: gerarID,
+            title,
+            description,
+            prep_time,
+            user_id,
+            modo_preparo
+        });
+        res.status(201).json("Recipe created successfully!");
+    }
+    catch (error) {
+        res.status(500).json({ message: 'An unexpected error occurred' });
     }
 }));
 app.get('/recipes/ingredients/:ingredients', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -82,44 +128,34 @@ app.get('/recipes/ingredients/:ingredients', (req, res) => __awaiter(void 0, voi
         res.status(500).json({ message: 'Erro ao buscar receitas por ingrediente', error: error.message });
     }
 }));
-app.delete('/recipes/:id_recipe', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { id_recipe } = req.params;
+app.get('/recipes/users/:username', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username } = req.params;
+    if (!username || typeof username !== 'string') {
+        res.status(400).json({ message: "Invalid username parameter." });
+        return;
+    }
     try {
-        const recipe = yield connection('recipes')
-            .where('id_recipe', id_recipe);
-        if (!recipe) {
-            res.status(404).json({ message: "recipe not found" });
+        const recipes = yield connection('recipes')
+            .join('users', 'recipes.user_id', '=', 'users.id_user')
+            .where('users.name_user', username)
+            .select('recipes.*');
+        if (recipes.length === 0) {
+            res.status(404).json({ message: "No recipes found for this user." });
+            return;
         }
-        yield connection('recipes')
-            .where('id_recipe', id_recipe)
-            .del();
-        res.status(200).json({ message: 'Receita deletada com sucesso!' });
+        res.status(200).json(recipes);
     }
     catch (error) {
-        res.status(500).json({ message: 'Erro ao deletar a receita', error: error.message });
-    }
-}));
-app.post('/recipes', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { title, description, prep_time, user_id, modo_preparo } = req.body;
-    const gerarID = Math.random();
-    try {
-        yield connection('recipes').insert({
-            id_recipe: gerarID,
-            title,
-            description,
-            prep_time,
-            user_id,
-            modo_preparo
-        });
-        res.status(201).json("Recipe created successfully!");
-    }
-    catch (error) {
-        res.status(500).json({ message: 'An unexpected error occurred', error: error.message });
+        res.status(500).json({ message: 'Error fetching recipes for user' });
     }
 }));
 app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const users = yield connection.select('*').from('users');
+        if (users.length === 0) {
+            res.status(400).json({ message: 'no users found' });
+            return;
+        }
         res.json(users);
     }
     catch (error) {
@@ -127,25 +163,105 @@ app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     }
 }));
 app.post('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { id_user, name_user, sobrenome, age, gender } = req.body;
-    if (!id_user || !name_user || !sobrenome || !age || !gender) {
-        res.send("campo obrigatório");
+    const { name_user, sobrenome, age, gender } = req.body;
+    const gerarID = (0, generatedId_1.generateId)();
+    if (!name_user || typeof name_user !== 'string' || name_user.length > 50) {
+        res.status(400).json({ message: "Invalid name_user" });
+        return;
+    }
+    if (!sobrenome || typeof sobrenome !== 'string' || sobrenome.length > 50) {
+        res.status(400).json({ message: "Invalid sobrenome" });
+        return;
+    }
+    if (typeof age !== 'number' || age <= 0) {
+        res.status(400).json({ message: "Invalid age" });
+        return;
+    }
+    if (!gender || typeof gender !== 'string' || gender.length > 12) {
+        res.status(400).json({ message: "Invalid gender" });
+        return;
     }
     try {
         yield connection('users').insert({
-            id_user,
+            id_user: gerarID,
             name_user,
             sobrenome,
             age,
             gender
         });
-        res.status(201).json('users created sucessfully!');
+        res.status(201).json('user created sucessfully!');
     }
     catch (error) {
         res.status(500).json({ message: 'something is wrong' });
     }
 }));
-app.delete('/usurs');
+app.delete('/users/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    if (!id) {
+        res.status(404).json({ message: "User ID is required." });
+        return;
+    }
+    try {
+        const userExists = yield connection('users').where('id_user', id).first();
+        if (!userExists) {
+            res.status(404).json({ message: "user not found" });
+            return;
+        }
+        yield connection('users')
+            .where('id_user', id)
+            .del();
+        res.status(200).json({ message: "user deleted sucessfully!" });
+    }
+    catch (error) {
+        res.status(500).json({ message: "An error occurred while deleting the user." });
+        return;
+    }
+}));
+app.put('/users/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const { name_user, sobrenome, age, gender } = req.body;
+    if (!id) {
+        res.status(404).json({ message: "User ID is required." });
+        return;
+    }
+    if (!name_user && !sobrenome && !age && !gender) {
+        res.status(400).json({ message: "At least one field is required for update." });
+        return;
+    }
+    if (name_user && typeof name_user !== 'string') {
+        res.status(400).json({ message: "name_user must be a string." });
+        return;
+    }
+    if (sobrenome && typeof sobrenome !== 'string') {
+        res.status(400).json({ message: "sobrenome must be a string." });
+        return;
+    }
+    if (age && (typeof age !== 'number' || age <= 0)) {
+        res.status(400).json({ message: "age must be a positive number." });
+        return;
+    }
+    if (gender && typeof gender !== 'string') {
+        res.status(400).json({ message: "gender must be a string." });
+        return;
+    }
+    try {
+        const userExsist = yield connection('users').where('id_user', id);
+        if (!userExsist) {
+            res.status(404).json({ message: "User not found." });
+            return;
+        }
+        yield connection('users').where('id_user', id).update({
+            name_user,
+            sobrenome,
+            age,
+            gender
+        });
+        res.status(200).json({ message: "User updated successfully!" });
+    }
+    catch (error) {
+        res.status(400).json({ message: "An error occurred while updating the user." });
+    }
+}));
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import knex, { Knex } from 'knex';
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 import { Request, Response } from 'express';
+import { generateId } from './generatedId';
 
 //servidor
 const app = express();
@@ -30,23 +31,37 @@ connection.raw('SELECT 1')
   });
 
 //BUSCAR TODAS AS RECEITAS (ok) 
-app.get('/recipes', async (req: Request, res: Response) => {
+//verifica se existe receitas(ok)
+app.get('/recipes', async (req: Request, res: Response) : Promise<void> => {
+
   try {
-    const recipe = await connection.select('*').from('recipes');
-    res.status(200).json(recipe);
+    const recipes = await connection.from('recipes');
+
+    if(recipes.length===0){
+      res.status(404).json({message: "no recipes found"})
+      return
+    }
+    res.status(200).json(recipes);
   } catch (err) {
-    res.status(500).json({ message: 'Erro ao buscar recipe', error: err });
+    res.status(500).json({ message: 'Erro ao buscar recipes', error: err });
   }
 });
 
 //BUSCAR RECEITAS COM TITULO ESPECÍFICO (ok)
-app.get('/recipes/:title', async (req: Request, res: Response) => {
+//verificar se o title foi inserido(ok)
+app.get('/recipes/:title', async (req: Request, res: Response) : Promise<void> => {
   const { title } = req.params
+
+  if(!title){
+    res.status(400).json({message: "Title is required"})
+  }
+
   try {
     const recipe = await connection('RECIPES').where('TITLE', 'ILIKE', `%${title}%`)//LIKE
 
-    if (!recipe.length) {
+    if (recipe.length===0) {
       res.status(404).json("recipe not found")
+      return
     }
     else {
       res.json(recipe)
@@ -57,46 +72,27 @@ app.get('/recipes/:title', async (req: Request, res: Response) => {
 });
 
 
-//preciso de ajuda, não sei usar o join
-app.get('/recipes/user/:username', async (req: Request, res: Response) => {
-  const { username } = req.params
-  try {
-    const recipe = await connection('recipes').join('USERS', 'RECIPE.USER_ID', 'USERS.ID_USERS').where('USERS.NAME_USER', username)
-      .select('RECIPES')
 
-    res.status(200).json(recipe)
-  } catch (error) {
-    res.status(500).json({ message: 'Erro ao bsucar recipe' })
-  }
-})
-
-
-//BUSCAR POR RECEITA QUE TENHA UM INGREDIENTE ESPECÍFICO
-//PROVÁVELMENRTE TEM QUE USAR O JOIN, AINDA NÃO SEI FAZER
-
-app.get('/recipes/ingredients/:ingredients', async (req: Request, res: Response) => {
-  const { ingredients } = req.params;
-    const newIngredients = req.body;
-  try {
-    const recipes = await connection('RECIPES')
-      .where('INGREDIENTS', 'ILIKE', `%${ingredients}%`);
-
-    res.status(200).json(recipes);
-  } catch (error) {
-    res.status(500).json({ message: 'Erro ao buscar receitas por ingrediente', error: (error as Error).message });
-  }
-});
-
-
-//TENHO QUE VERIFICAR O ID DA RECEITA PRA PODER EXCLUIR 
 // Rota para excluir receita por ID (ok)
-app.delete('/recipes/:id_recipe', async (req: Request, res: Response) => {
+//verificar se o id foi inserido(ok)
+//verificar se a receita existe(ok)
+
+app.delete('/recipes/:id_recipe', async (req: Request, res: Response): Promise<void> => {
+
   const { id_recipe } = req.params;
+
+  if(!id_recipe){
+    res.status(404).json({message: "Id is required"})
+  }
+
   try {
+
     const recipe = await connection('recipes')
-      .where('id_recipe', id_recipe)  
-    if(!recipe){
-      res.status(404).json({message: "recipe not found"})
+      .where('id_recipe', id_recipe).first();
+
+    if (!recipe) {
+      res.status(404).json({ message: "Recipe not found" })
+      return
     }
 
     //deleta
@@ -105,18 +101,37 @@ app.delete('/recipes/:id_recipe', async (req: Request, res: Response) => {
       .del();
 
     res.status(200).json({ message: 'Receita deletada com sucesso!' });
+
   } catch (error) {
-     res.status(500).json({ message: 'Erro ao deletar a receita', error: (error as Error).message });
+    res.status(500).json({ message: 'Erro ao deletar a receita', error: (error as Error).message });
   }
 });
 
 
 
 // CRIAR RECEITA (ok)
-app.post('/recipes', async (req: Request, res: Response) => {
+//* verificar se já existe user(ok)
+//somente um usuário logado pode criar receita falta
+//verificar se todos os campos foram inseridos(ok)
+
+app.post('/recipes', async (req: Request, res: Response) : Promise<void> => {
   const { title, description, prep_time, user_id, modo_preparo } = req.body;
-  const gerarID = Math.random();
+  const gerarID = generateId(); // Chamar a função para gerar um ID
+
+if(!title || !description || !prep_time || !user_id || !modo_preparo){
+  res.status(404).json({message: "Campo obrigatório"})
+  return
+}
+
   try {
+    // Verifica se o user_id existe
+    const userExists = await connection('users').where('id_user', user_id).first();
+
+    if (!userExists) {
+      res.status(404).json({ message: 'User not found' });
+      return
+    }
+
     await connection('recipes').insert({
       id_recipe: gerarID,
       title,
@@ -128,54 +143,230 @@ app.post('/recipes', async (req: Request, res: Response) => {
 
     res.status(201).json("Recipe created successfully!");
   } catch (error) {
-    res.status(500).json({ message: 'An unexpected error occurred', error: (error as Error).message });
+    res.status(500).json({ message: 'An unexpected error occurred' });
   }
 });
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//BUSCAR POR RECEITA QUE TENHA UM INGREDIENTE ESPECÍFICO
+//PROVÁVELMENRTE TEM QUE USAR O JOIN, AINDA NÃO SEI FAZER
+
+app.get('/recipes/ingredients/:ingredients', async (req: Request, res: Response): Promise<void> => {
+  const { ingredients } = req.params;
+  
+  const newIngredients = req.body;
+  try {
+    const recipes = await connection('RECIPES')
+      .where('INGREDIENTS', 'ILIKE', `%${ingredients}%`);
+
+    res.status(200).json(recipes);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar receitas por ingrediente', error: (error as Error).message });
+  }
+});
+
+//buscar receitas de um usuário específico
+//verificar se o nome foi inserido e se ele é do tipo string(ok)
+//verificar se existe receitas (ok)
+
+
+app.get('/recipes/users/:username', async (req: Request, res: Response) => {
+  const { username } = req.params;
+
+  if (!username || typeof username !== 'string') {
+    res.status(400).json({ message: "Invalid username parameter." });
+    return;
+  }
+
+  try {
+    const recipes = await connection('recipes')
+      .join('users', 'recipes.user_id', '=', 'users.id_user')
+      .where('users.name_user', username)
+      .select('recipes.*');
+
+    if (recipes.length === 0) {
+      res.status(404).json({ message: "No recipes found for this user." });
+      return;
+    }
+
+    res.status(200).json(recipes);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching recipes for user'});
+  }
+});
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 //ENDPOINTS DO USERS
 
 
-//BUSCAR TODOS OS USUÁRIOS (ok)
-app.get('/users', async (req: Request, res: Response )=>{
-  try{
+//BUSCAR TODOS OS USUÁRIOS 
+//verifica se existem usuários(ok)
+app.get('/users', async (req: Request, res: Response): Promise<void> => {
+
+  try {
     const users = await connection.select('*').from('users');
+
+    if (users.length === 0) {
+      res.status(400).json({ message: 'no users found' })
+      return
+    }
     res.json(users);
-  }catch(error){
-    res.status(404).json({message: 'users not found'})
+  } catch (error) {
+    res.status(404).json({ message: 'users not found' })
   }
 })
 
 //CRIAR UM USUÁRIO (ok)
-//tratar os erros
+//verifica se os campos estão preenchidos e seguem o padrão das tabelas (ok)
+app.post('/users', async (req: Request, res: Response): Promise<void> => {
+  const { name_user, sobrenome, age, gender } = req.body;
 
-app.post('/users', async (req: Request, res: Response)=>{
-  const { id_user, name_user, sobrenome, age, gender} = req.body;
+  const gerarID = generateId();
+
   
-  if(!id_user  || !name_user || !sobrenome || !age || !gender){
-     res.status(500).json("campo obrigatório")
+   if (!name_user || typeof name_user !== 'string' || name_user.length > 50) {
+    res.status(400).json({ message: "Invalid name_user" });
+    return;
+  }
+
+  if (!sobrenome || typeof sobrenome !== 'string' || sobrenome.length > 50) {
+    res.status(400).json({ message: "Invalid sobrenome" });
+    return;
+  }
+
+  if (typeof age !== 'number' || age <= 0) {
+    res.status(400).json({ message: "Invalid age" });
+    return;
+  }
+
+  if (!gender || typeof gender !== 'string' || gender.length > 12) {
+    res.status(400).json({ message: "Invalid gender" });
+    return;
   }
   try {
     await connection('users').insert({
-      id_user,
+      id_user: gerarID,
       name_user,
       sobrenome,
       age,
       gender
     })
 
-   res.status(201).json('users created sucessfully!')
+    res.status(201).json('user created sucessfully!')
   } catch (error) {
-   res.status(500).json({message: 'something is wrong'})
+    res.status(500).json({ message: 'something is wrong' })
+  }
+
+});
+
+
+//DELETAR UM USUÁRIO
+//id inserido (ok)
+//se o usuário existe(ok)
+
+app.delete('/users/:id', async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params;
+
+
+  if (!id) {
+    res.status(404).json({ message: "User ID is required." })
+    return
+  }
+
+  try {
+
+    const userExists = await connection('users').where('id_user', id).first()
+
+    if (!userExists) {
+      res.status(404).json({ message: "user not found" })
+      return
+    }
+
+    await connection('users')
+      .where('id_user', id)
+      .del();
+
+    res.status(200).json({ message: "user deleted sucessfully!" })
+  }
+  catch (error) {
+    res.status(500).json({ message: "An error occurred while deleting the user." });
+    return
+  }
+
+});
+
+
+//atualizar um usuário
+//verificar id(ok)
+//verificar itens de atualização e seus tipos(ok)
+//verificar se o usuários existe no banco de dados(ok)
+app.put('/users/:id', async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params
+  const { name_user, sobrenome, age, gender } = req.body
+
+  if (!id) {
+    res.status(404).json({ message: "User ID is required." })
+    return
+  }
+
+  if (!name_user && !sobrenome && !age && !gender) {
+    res.status(400).json({ message: "At least one field is required for update." });
+    return;
+  }
+
+  if (name_user && typeof name_user !== 'string') {
+    res.status(400).json({ message: "name_user must be a string." });
+    return;
+  }
+  
+  if (sobrenome && typeof sobrenome !== 'string') {
+    res.status(400).json({ message: "sobrenome must be a string." });
+    return;
+  }
+
+  if (age && (typeof age !== 'number' || age <= 0)) {
+    res.status(400).json({ message: "age must be a positive number." });
+    return;
+  }
+
+  if (gender && typeof gender !== 'string') {
+    res.status(400).json({ message: "gender must be a string." });
+    return;
+  }
+
+  try {
+
+    const userExsist = await connection('users').where('id_user', id)
+
+    if (!userExsist) {
+      res.status(404).json({ message: "User not found." })
+      return
+    }
+
+    await connection('users').where('id_user', id).update({
+      name_user,
+      sobrenome,
+      age,
+      gender
+    })
+
+    res.status(200).json({ message: "User updated successfully!" })
+
+  } catch (error) {
+    res.status(400).json({ message: "An error occurred while updating the user." })
   }
 
 })
-//DELETAR UM USUÁRIO
-
-app.delete('/usurs')
-
 
 // Configurando o servidor para escutar na porta 3000
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
 //
+
+
